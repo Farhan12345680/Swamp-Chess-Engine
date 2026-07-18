0bfab9abcf12431dbe9a2082736d225d14223eeb
JogindarJeherila <risadjabin@gmail.com>
2026-07-18 13:34:58 +0600

    commit FEN

---
+++ Swamp Server/Board Representation/Common.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdint.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define MAX_UCI_QUERY_LENGTH 257
+
+#ifndef __uint64_t
+typedef uint64_t __uint64_t;
+#endif
