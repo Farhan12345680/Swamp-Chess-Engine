e76c733cb4a996d43932f87a35015fa67f741c88
JogindarJeherila <risadjabin@gmail.com>
2026-07-25 12:32:06 +0600

    Basic Server and README.md Update (#2)
    * Readme Update
    
    * README Update
    
    * Java Server
    
    * commit inbetween

--- Swamp Server/Swamp.c
+++ Swamp Server/Swamp.c
@@ -1,16 +1,27 @@
-#include "Board Representation/bitboards.h";
-#include "Board Representation/FEN_parser.h";
-#include "Board Representation/gameHistory.h";
+#include "../Board Representation/BoardRepresentation.h";
+#include <stdio.h>;
 
+int main()
+{
+    // DO initialization
 
+    // Server Reading
 
-int main(){
-    GameState _globalGame = initiaizeNewGame();
+    while (1)
+    {
+        char *_str = (char *)malloc(sizeof(char) * 200);
 
-    printf("%s\n" ,fenStringGenerationFromGameState(&_globalGame) );
+        scanf("%s", _str);
 
-    
+        _str[strcspn(_str, "\r\n")] = '\0';
 
-    
-
+        if (strcmp(_str, "uci") == 0)
+        {
+        }
+        else
+        {
+            free(_str);
+            break;
+        }
+    }
 }
